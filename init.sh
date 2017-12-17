e015e260f1ae8103dc29b3070b25a3e3d6c3c8ca
Andrii Holub <andr.holub@gmail.com>
2017-12-17 21:44:28 +0000

    gunicorn

--- init.sh
+++ init.sh
@@ -3,3 +3,4 @@ sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 sudo /etc/init.d/gunicorn restart
+gunicorn -w 4 hello:app
