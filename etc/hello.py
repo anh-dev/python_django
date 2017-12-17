5ee45d29ebfa0f22a046cc3a77ab0d63394743ec
Andrii Holub <andr.holub@gmail.com>
2017-12-17 21:51:38 +0000

    changes

--- etc/hello.py
+++
@@ -1,4 +0,0 @@
-import multiproccessing
-
-bind = '0.0.0.0:8080'
-workers = multiproccessing.cpu_count() * 2 + 1
--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
-sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
+sudo ln -sf /home/box/etc/gunicorn.conf  /etc/gunicorn.d/test
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
